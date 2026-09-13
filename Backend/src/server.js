65ce64f91b6b85ab4e9b4d6365e55a93934924ee
Anubhav-Singh01 <anshanu2580singh@gmail.com>
2026-09-13 18:47:50 +0530

    error fix3

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -24,7 +24,7 @@ app.use(rateLimiter);
 
 app.use("/api/notes",noteRoute)
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     });
